e2509d2bf655326b64a55ea0a3321b0057cc148b
SHANICERUGURU <rugurushanice990@gmail.com>
2024-05-14 13:53:09 +0300

    control structures

---
+++ js/controlstructures.js
@@ -0,0 +1,50 @@
+{
+    let number=50;
+
+    if( number%3==0){
+        console.log(number+"divisible by three")
+    }else{
+        console.log(number+" not divisible by three")
+
+    }
+}
+
+{
+    let number=55;
+    
+    if(number%2==0){
+        console.log(number+" is even")
+    }else{
+        console.log(number+ " is odd")
+    }
+}
+
+{
+    let grade;
+    let marks=70;
+    if (marks>=85){
+        grade="A"
+    }
+       else if (marks<=84) {
+        grade="B"
+       }
+       else if (marks<=70){
+        grade="C"
+       }
+       else if (marks<=60 ){
+        grade="D"
+       }
+       else{
+        grade="E"
+       }
+       console.log(grade)
+}
+
+{
+    let number=2024;
+    if (number%4==0){
+        console.log(number+" leap year")
+    }else{
+        console.log(number+" not a leap year")
+    }
+}
---
+++ js/datatypes.js
@@ -0,0 +1,19 @@
+{
+    // let name="james"+"jones";
+    // console.log (name);
+    // console.log ("\n");
+
+    // console.log(10!=10);
+    // console.log (Boolean(0));
+
+    // console.log ("a"> "b");
+    // console.log("5"===5)}
+
+    {
+    let x=6;
+        if (x< 10){
+        console.log ("x is less than 10")
+    }else{
+
+    } 
+}
