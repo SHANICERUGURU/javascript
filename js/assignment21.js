995c5c00336ed082da750853bba4b1cecc6d9294
SHANICERUGURU <rugurushanice990@gmail.com>
2024-05-21 10:08:31 +0300

    makechange

--- js/assignment21.js
+++ js/assignment21.js
@@ -3,12 +3,38 @@
         let string = str.toLowerCase();
         for(let i=[0];i<string.length/2;++i){
             if(string[i] == string[string.length - 1 - i])
-                console.log("false")
-            else(
-                 console.log("true")
-            )
+                return true
+            else
+                 return false
+            
         }
     }
-     const string="a man, a plan, a canal, panama"
+     const string="dad"
       console.log(palindromechecker(string))
-} 
+}
+
+
+{
+    function numbertriangle(x) {
+        for (let i = 1; i <= x; i++) {
+            // console.log(x);
+            let row = '';
+            for (let y = 1; y <= i; y++) {
+                row=x+y;
+            } console.log(row);
+        }
+    } numbertriangle(10)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
